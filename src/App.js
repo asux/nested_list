c62d448bd334b3c5013cea1271058315dc882db8
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2020-05-05 20:37:10 +0300

    Implement items swapping

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import List from './components/List'
 import './App.css';
 
 const TestItems = [
-  { id: 1, text: 'Coffee', position: 0 },
-  { id: 2, text: 'Tea', position: 1, items: [
-    { id: 3, text: 'Black tea', position: 0 },
-    { id: 4, text: 'Green tea', position: 1 },
+  { id: 1, text: 'Coffee', position: 1 },
+  { id: 2, text: 'Tea', position: 2, items: [
+    { id: 3, text: 'Black tea', position: 1 },
+    { id: 4, text: 'Green tea', position: 2 },
   ]},
-  { id: 5, text: 'Milk', position: 2 }
+  { id: 5, text: 'Milk', position: 3 }
 ]
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <h2>A Nested List Editor</h2>
       <div>
-        <List items={TestItems} />
+        <List items={TestItems} position={0} lastPosition={3}/>
       </div>
     </div>
   );
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,42 +1,143 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ListItem from './ListItem';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { cloneDeep, sortBy } from "lodash";
 
-class List extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: props.items };
+function List(props) {
+  const [items, setItems] = useState(props.items);
+  const [newItemText, setNewItemText] = useState("");
+
+  const root = props.position === 0;
+  const first = props.position === 1;
+  const last = props.position === props.lastPosition;
+
+  let sublistButton;
+
+  function handleMoveUp(itemId) {
+    return (e) => {
+      e.preventDefault();
+
+      let newItems = cloneDeep(items);
+      let current = newItems.find((i) => i.id === itemId);
+      let prev = newItems.find((i) => i.position === current.position - 1);
+      current.position -= 1;
+      current.lastPosition = newItems.length + 1;
+      if (prev) {
+        prev.position += 1;
+        prev.lastPosition = newItems.length + 1;
+      }
+      newItems = sortBy(newItems, "position");
+      setItems(newItems);
+    };
   }
 
-  defaultHandler = (e) => {
-    console.debug(e);
+  function handleMoveDown(itemId) {
+    return (e) => {
+      e.preventDefault();
+
+      let newItems = cloneDeep(items);
+      let current = newItems.find((i) => i.id === itemId);
+      let next = newItems.find((i) => i.position === current.position + 1);
+      current.position += 1;
+      current.lastPosition = newItems.length + 1;
+      if (next) {
+        next.position -= 1;
+        next.lastPosition = newItems.length + 1;
+      }
+      newItems = sortBy(newItems, "position");
+      setItems(newItems);
+    };
   }
 
-  render () {
-    const { items } = this.state;
-    return (
-      <ul className="list">
-        {items.map((item, index, list) => <ListItem
-          text={item.text}
-          items={item.items}
-          hasUpButton={index > 0}
-          hasDownButton={index < list.length}
-          handleMoveUp={this.defaultHandler}
-          handleDownUp={this.defaultHandler}
-          handleRemove={this.defaultHandler}
-          key={item.id}
-        />)}
-        <li>
-          <input type="text"></input>
-          <button>Add</button>
-        </li>
-      </ul>
-    );
-  };
-};
+  function handleAddItems(e) {
+    e.preventDefault();
+    setItems([{ id: new Date(), position: 0 }]);
+  }
+
+  function handleRemoveItems(e) {
+    e.preventDefault();
+    setItems([]);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let newItems = cloneDeep(items);
+    newItems.push({
+      id: new Date(),
+      position: newItems.length + 1,
+      text: newItemText,
+    });
+    console.debug(newItems);
+    setItems(newItems);
+  }
+
+  function handleRemove(itemId) {
+    return (e) => {
+      setItems(items.filter((item) => item.id !== itemId));
+    };
+  }
+
+  if (items && items.length > 0) {
+    sublistButton = <button onClick={handleRemoveItems}>Remove Sublist</button>;
+  } else {
+    sublistButton = <button onClick={handleAddItems}>Add Sublist</button>;
+  }
+
+  const controls = (
+    <>
+      <span>{props.text}</span>
+      {first || <button onClick={props.handleMoveUp}>&uarr;</button>}
+      {last || <button onClick={props.handleMoveDown}>&darr;</button>}
+      {sublistButton}
+      <button onClick={props.handleRemove}>Remove</button>
+    </>
+  );
+
+  return (
+    <>
+      {root || controls}
+      {items && items.length > 0 && (
+        <ul>
+          {items.map((item, index, list) => (
+            <li>
+              <List
+                text={item.text}
+                items={item.items}
+                position={item.position}
+                lastPosition={list.length}
+                key={item.id}
+                handleRemove={handleRemove(item.id)}
+                handleMoveUp={handleMoveUp(item.id)}
+                handleMoveDown={handleMoveDown(item.id)}
+              />
+            </li>
+          ))}
+          <li>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="add-item"
+                value={newItemText}
+                onChange={(e) => setNewItemText(e.target.value)}
+              ></input>
+              <button type="submit">Add</button>
+            </form>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+}
 
 List.propTypes = {
-  items: PropTypes.array
-}
+  text: PropTypes.string,
+  items: PropTypes.array,
+  position: PropTypes.number.isRequired,
+  lastPosition: PropTypes.number,
+  id: PropTypes.string,
+  handleMoveUp: PropTypes.func,
+  handleDownUp: PropTypes.func,
+  handleRemove: PropTypes.func,
+};
 
 export default List;
